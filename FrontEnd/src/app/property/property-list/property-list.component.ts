1de5fe3e38fd140710dbde878cf01b9b07916d84
Pranav sapate <pranavsapate9767@gmail.com>
2023-12-07 00:57:12 +0530

    Changes in property list

--- FrontEnd/src/app/property/property-list/property-list.component.ts
+++ FrontEnd/src/app/property/property-list/property-list.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class PropertyListComponent {
 
+  properties : Array<any> =[
+    {
+    "Id":1,
+    "Name":"Birla House",
+    "Type":"House",
+    "Price":12000
+    },
+    {
+      "Id":2,
+      "Name":"Adani House",
+      "Type":"House",
+      "Price":123000
+      },
+      {
+        "Id":3,
+        "Name":"Ambani House",
+        "Type":"House",
+        "Price":144000
+        },
+        {
+          "Id":4,
+          "Name":"Tata House",
+          "Type":"House",
+          "Price":19000
+          },
+          {
+            "Id":5,
+            "Name":"Macro House",
+            "Type":"House",
+            "Price":11000
+            },
+            {
+              "Id":6,
+              "Name":"Pearl white House",
+              "Type":"House",
+              "Price":21000
+              },
+]
 }
